3516f146f733c7860c967272d4e71dfde76a9b57
( DO DREAM DEVELOPMENT) DEVELOPER D 실행하고 꿈꾸고 발전하는개발자.. 가 되고싶습니다 <68416184+silver-liq9118@users.noreply.github.com>
2023-11-20 15:43:01 +0900

    update path

--- app.py
+++ app.py
@@ -16,15 +16,17 @@ class Main:
 
     def load_config(self):
         self.config = configparser.ConfigParser()
-        self.config.read('config.ini')
-        self.mobsf_path = self.config['MobSF'].get('MobSF', self.config['DEFAULT']['MobSF'])
+        config_file_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+        
+        self.config.read(config_file_path)
+        self.mobsf_path = os.path.join(os.path.dirname(__file__), self.config['MobSF']['MobSF'])
         self.server_ip = self.config['SERVER'].get('ServerIP', self.config['DEFAULT']['ServerIP'])
         self.api_key = self.config['API'].get('ApiKey', self.config['DEFAULT']['ApiKey'])
         self.file_path = self.config['FILE'].get('FilePath', self.config['DEFAULT']['FilePath']).split(',')
         self.avm_name = self.config['AVM'].get('AVM_Name', self.config['DEFAULT']['AVM_Name'])
         self.frida_script_path = self.config['Frida'].get('Frida_Script', self.config['DEFAULT']['Frida_Script'])
-        self.encryption_method = self.config['Encryption_method'].get('encryption_method', self.config['DEFAULT']['Encryption_method'])
-        
+        self.encryption_method = self.config['Encryption_method'].get('encryption_method', self.config['DEFAULT']['Encryption_method'])   
+
     def save_config(self):
         with open('config.ini', 'w') as configfile:
             self.config.write(configfile)
